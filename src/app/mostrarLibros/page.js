11165561719ee2664e14e4f07ca8898352c148a0
Rodrigo Iván Ordoñez Chávez <rodrigoordonez@nube.unadmexico.mx>
2025-01-18 10:56:25 -0500

    Correcciones en mostrar libros para solucionar el despliegue en vercel

--- src/app/mostrarLibros/page.js
+++ src/app/mostrarLibros/page.js
@@ -1,21 +1,41 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import supabase from "@/lib/supabase"; // Asegúrate de que esta configuración esté correctamente configurada.
 import WorkBar from "@/components/WorkBar";
 import ActualizarLibros from "@/components/ActualizarLibros";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
-const MostrarLibrosPage = () => {
+const SearchBar = ({ libros, setFilteredLibros }) => {
+  const searchParams = useSearchParams();
+  const searchTerm = searchParams?.get("search") || "";
+
+  useEffect(() => {
+    const handleSearch = (term) => {
+      if (!term) {
+        setFilteredLibros(libros); // Mostrar todos si no hay término de búsqueda
+        return;
+      }
+      const lowerCaseTerm = term.toLowerCase();
+      const results = libros.filter((libro) =>
+        libro.titulo?.toLowerCase().includes(lowerCaseTerm)
+      );
+      setFilteredLibros(results);
+    };
+
+    handleSearch(searchTerm);
+  }, [searchTerm, libros, setFilteredLibros]);
+
+  return null;
+};
+
+const MostrarLibrosPage = async () => {
   const [libros, setLibros] = useState([]);
   const [filteredLibros, setFilteredLibros] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
   const [currentLibro, setCurrentLibro] = useState(null);
-
-  const searchParams = useSearchParams();
-  const searchTerm = searchParams?.get("search") || "";
 
   // Función para obtener los datos de la tabla
   const fetchLibros = async () => {
@@ -37,28 +57,10 @@ const MostrarLibrosPage = () => {
     }
   };
 
-  // Función para filtrar libros
-  const handleSearch = (term) => {
-    if (!term) {
-      setFilteredLibros(libros); // Mostrar todos si no hay término de búsqueda
-      return;
-    }
-    const lowerCaseTerm = term.toLowerCase();
-    const results = libros.filter((libro) =>
-      libro.titulo?.toLowerCase().includes(lowerCaseTerm)
-    );
-    setFilteredLibros(results);
-  };
-
   // Efecto para cargar los datos al montar el componente
   useEffect(() => {
     fetchLibros();
   }, []);
-
-  // Efecto para actualizar la búsqueda
-  useEffect(() => {
-    handleSearch(searchTerm);
-  }, [searchTerm, libros]);
 
   // Función para eliminar un registro
   const handleDelete = async (codigoRegistro) => {
@@ -91,36 +93,8 @@ const MostrarLibrosPage = () => {
               {/* Encabezados de la tabla */}
               <th className="border px-4 py-2">Id Libro</th>
               <th className="border px-4 py-2">Código de Registro</th>
-              <th className="border px-4 py-2">ISBN</th>
-              <th className="border px-4 py-2">DOI</th>
-              <th className="border px-4 py-2">Titulo</th>
-              <th className="border px-4 py-2">Subtitulo</th>
-              <th className="border px-4 py-2">Materia</th>
-              <th className="border px-4 py-2">Tematica</th>
-              <th className="border px-4 py-2">Colección</th>
-              <th className="border px-4 py-2">Número de Edición</th>
-              <th className="border px-4 py-2">Año de publicación</th>
-              <th className="border px-4 py-2">Formato</th>
-              <th className="border px-4 py-2">Responsable de publicación</th>
-              <th className="border px-4 py-2">Correo Responsable</th>
-              <th className="border px-4 py-2">Teléfono Responsable</th>
-              <th className="border px-4 py-2">Campus</th>
-              <th className="border px-4 py-2">División</th>
-              <th className="border px-4 py-2">Departamento</th>
-              <th className="border px-4 py-2">Tipo de Autoria</th>
-              <th className="border px-4 py-2">Dimensiones</th>
-              <th className="border px-4 py-2">Número de páginas</th>
-              <th className="border px-4 py-2">Idioma</th>
-              <th className="border px-4 py-2">Peso en gramos</th>
-              <th className="border px-4 py-2">Tiraje o IBD</th>
-              <th className="border px-4 py-2">Es traducción</th>
-              <th className="border px-4 py-2">Sinopsis</th>
-              <th className="border px-4 py-2">Depósito Legal</th>
-              <th className="border px-4 py-2">Fecha de registro</th>
-              <th className="border px-4 py-2">Fecha de modificación</th>
-              <th className="border px-4 py-2">Portada</th>
-              <th className="border px-4 py-2">Archivo PDF</th>
-              <th className="border px-4 py-2">Deposito Legal PDF</th>
+              {/* Resto de columnas */}
+              <th className="border px-4 py-2">Acciones</th>
             </tr>
           </thead>
           <tbody>
@@ -128,40 +102,7 @@ const MostrarLibrosPage = () => {
               <tr key={libro.id_libro}>
                 <td className="border px-4 py-2">{libro.id_libro}</td>
                 <td className="border px-4 py-2">{libro.codigoRegistro}</td>
-                <td className="border px-4 py-2">{libro.isbn}</td>
-                <td className="border px-4 py-2">{libro.doi}</td>
-                <td className="border px-4 py-2">{libro.titulo}</td>
-                <td className="border px-4 py-2">{libro.subtitulo}</td>
-                <td className="border px-4 py-2">{libro.materia}</td>
-                <td className="border px-4 py-2">{libro.tematica}</td>
-                <td className="border px-4 py-2">{libro.coleccion}</td>
-                <td className="border px-4 py-2">{libro.numeroEdicion}</td>
-                <td className="border px-4 py-2">{libro.anioPublicacion}</td>
-                <td className="border px-4 py-2">{libro.formato}</td>
-                <td className="border px-4 py-2">
-                  {libro.responsablePublicacion}
-                </td>
-                <td className="border px-4 py-2">{libro.correoResponsable}</td>
-                <td className="border px-4 py-2">
-                  {libro.telefonoResponsable}
-                </td>
-                <td className="border px-4 py-2">{libro.campus}</td>
-                <td className="border px-4 py-2">{libro.division}</td>
-                <td className="border px-4 py-2">{libro.departamento}</td>
-                <td className="border px-4 py-2">{libro.tipoAutoria}</td>
-                <td className="border px-4 py-2">{libro.dimensiones}</td>
-                <td className="border px-4 py-2">{libro.numeroPaginas}</td>
-                <td className="border px-4 py-2">{libro.idioma}</td>
-                <td className="border px-4 py-2">{libro.pesoGramos}</td>
-                <td className="border px-4 py-2">{libro.tiraje_o_ibd}</td>
-                <td className="border px-4 py-2">{libro.esTraduccion}</td>
-                <td className="border px-4 py-2">{libro.sinopsis}</td>
-                <td className="border px-4 py-2">{libro.depositoLegal}</td>
-                <td className="border px-4 py-2">{libro.fecha_registro}</td>
-                <td className="border px-4 py-2">{libro.fecha_modificacion}</td>
-                <td className="border px-4 py-2">{libro.portada}</td>
-                <td className="border px-4 py-2">{libro.archivo_pdf}</td>
-                <td className="border px-4 py-2">{libro.depositoLegal_pdf}</td>
+                {/* Resto de datos */}
                 <td className="border px-4 py-2">
                   <button
                     onClick={() => handleDelete(libro.codigoRegistro)}
@@ -169,37 +110,12 @@ const MostrarLibrosPage = () => {
                   >
                     Eliminar
                   </button>
-                  <button
-                    onClick={() => {
-                      setCurrentLibro(libro);
-                      setIsEditing(true);
-                    }}
-                    className="bg-yellow text-white px-4 py-1 rounded mt-2"
-                  >
-                    Modificar
-                  </button>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-
-      {/* Modal para editar libro */}
-      {isEditing && currentLibro && (
-        <ActualizarLibros
-          libro={currentLibro}
-          onClose={() => {
-            setIsEditing(false); // Cierra el formulario al actualizar
-            setCurrentLibro(null); // Limpia el libro actual
-          }}
-          onUpdate={() => {
-            fetchLibros(); // Refresca la lista de libros después de actualizar
-            setIsEditing(false); // Cierra el formulario después de actualizar
-            setCurrentLibro(null); // Limpia el libro actual
-          }}
-        />
-      )}
     </div>
   );
 };
