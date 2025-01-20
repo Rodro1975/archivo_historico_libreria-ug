f690b4b47932686433665a1f670a464683b3824b
Rodrigo Iván Ordoñez Chávez <rodrigoordonez@nube.unadmexico.mx>
2025-01-20 13:41:55 -0500

    se integra el uso de suspend para solucionar despliegues en vercel

--- src/app/mostrarUsuarios/page.js
+++ src/app/mostrarUsuarios/page.js
@@ -1,6 +1,6 @@
 "use client"; // Asegúrate de que el componente se ejecute solo en el cliente
 
-import { useEffect, useState, useCallback } from "react"; 
+import { useEffect, useState, useCallback, Suspense } from "react"; 
 import supabase from "@/lib/supabase";
 import WorkBar from "@/components/WorkBar";
 import ActualizarUsuarios from "@/components/ActualizarUsuarios"; 
@@ -67,84 +67,87 @@ const MostrarUsuariosPage = () => {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div className="min-h-screen bg-blue">
-      <WorkBar />
-      <h1 className="text-4xl text-yellow text-center font-bold mt-8 mb-8">
-        Lista de Usuarios
-      </h1>
-      <div className="overflow-x-auto w-full max-w-screen-lg mx-auto px-4">
-        <table className="min-w-full bg-white border border-gray-300 text-blue mb-8">
-          <thead>
-            <tr>
-              <th className="border px-4 py-2">Id Usuario</th>
-              <th className="border px-4 py-2">Primer Nombre</th>
-              <th className="border px-4 py-2">Segundo Nombre</th>
-              <th className="border px-4 py-2">Apellido Paterno</th>
-              <th className="border px-4 py-2">Apellido Materno</th>
-              <th className="border px-4 py-2">Email</th>
-              <th className="border px-4 py-2">Teléfono</th>
-              <th className="border px-4 py-2">Justificación</th>
-              <th className="border px-4 py-2">Password</th>
-              <th className="border px-4 py-2">Rol</th>
-              <th className="border px-4 py-2">Foto</th>
-              <th className="border px-4 py-2">Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredUsuarios.map((usuario) => (
-              <tr key={usuario.id_usuario}>
-                <td className="border px-4 py-2">{usuario.id_usuario}</td>
-                <td className="border px-4 py-2">{usuario.primer_nombre}</td>
-                <td className="border px-4 py-2">{usuario.segundo_nombre}</td>
-                <td className="border px-4 py-2">{usuario.apellido_paterno}</td>
-                <td className="border px-4 py-2">{usuario.apellido_materno}</td>
-                <td className="border px-4 py-2">{usuario.email}</td>
-                <td className="border px-4 py-2">{usuario.telefono}</td>
-                <td className="border px-4 py-2">{usuario.justificacion}</td>
-                <td className="border px-4 py-2">{usuario.password}</td>
-                <td className="border px-4 py-2">{usuario.role}</td>
-                <td className="border px-4 py-2">{usuario.foto}</td>
-                <td className="border px-4 py-2">
-                  <button
-                    onClick={() => handleDelete(usuario.id_usuario)}
-                    className="bg-red-500 text-white px-4 py-1 rounded"
-                  >
-                    Eliminar
-                  </button>
-                  <button
-                    onClick={() => {
-                      setCurrentUsuario(usuario); 
-                      setIsEditing(true);
-                    }}
-                    className="bg-yellow text-white px-4 py-1 rounded mt-2"
-                  >
-                    Modificar
-                  </button>
-                </td>
+    <Suspense fallback={<p>Cargando...</p>}>
+      <div className="min-h-screen bg-blue">
+        <WorkBar />
+        <h1 className="text-4xl text-yellow text-center font-bold mt-8 mb-8">
+          Lista de Usuarios
+        </h1>
+        <div className="overflow-x-auto w-full max-w-screen-lg mx-auto px-4">
+          <table className="min-w-full bg-white border border-gray-300 text-blue mb-8">
+            <thead>
+              <tr>
+                <th className="border px-4 py-2">Id Usuario</th>
+                <th className="border px-4 py-2">Primer Nombre</th>
+                <th className="border px-4 py-2">Segundo Nombre</th>
+                <th className="border px-4 py-2">Apellido Paterno</th>
+                <th className="border px-4 py-2">Apellido Materno</th>
+                <th className="border px-4 py-2">Email</th>
+                <th className="border px-4 py-2">Teléfono</th>
+                <th className="border px-4 py-2">Justificación</th>
+                <th className="border px-4 py-2">Password</th>
+                <th className="border px-4 py-2">Rol</th>
+                <th className="border px-4 py-2">Foto</th>
+                <th className="border px-4 py-2">Acciones</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {filteredUsuarios.map((usuario) => (
+                <tr key={usuario.id_usuario}>
+                  <td className="border px-4 py-2">{usuario.id_usuario}</td>
+                  <td className="border px-4 py-2">{usuario.primer_nombre}</td>
+                  <td className="border px-4 py-2">{usuario.segundo_nombre}</td>
+                  <td className="border px-4 py-2">{usuario.apellido_paterno}</td>
+                  <td className="border px-4 py-2">{usuario.apellido_materno}</td>
+                  <td className="border px-4 py-2">{usuario.email}</td>
+                  <td className="border px-4 py-2">{usuario.telefono}</td>
+                  <td className="border px-4 py-2">{usuario.justificacion}</td>
+                  <td className="border px-4 py-2">{usuario.password}</td>
+                  <td className="border px-4 py-2">{usuario.role}</td>
+                  <td className="border px-4 py-2">{usuario.foto}</td>
+                  <td className="border px-4 py-2">
+                    <button
+                      onClick={() => handleDelete(usuario.id_usuario)}
+                      className="bg-red-500 text-white px-4 py-1 rounded"
+                    >
+                      Eliminar
+                    </button>
+                    <button
+                      onClick={() => {
+                        setCurrentUsuario(usuario); 
+                        setIsEditing(true);
+                      }}
+                      className="bg-yellow text-white px-4 py-1 rounded mt-2"
+                    >
+                      Modificar
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
 
-      {isEditing && currentUsuario && (
-        <ActualizarUsuarios
-          usuario={currentUsuario}
-          onClose={() => {
-            setIsEditing(false);
-            setCurrentUsuario(null);
-          }}
-          onUpdate={() => {
-            fetchUsuarios();
-            setIsEditing(false);
-            setCurrentUsuario(null);
-          }}
-        />
-      )}
-    </div>
+        {isEditing && currentUsuario && (
+          <ActualizarUsuarios
+            usuario={currentUsuario}
+            onClose={() => {
+              setIsEditing(false);
+              setCurrentUsuario(null);
+            }}
+            onUpdate={() => {
+              fetchUsuarios();
+              setIsEditing(false);
+              setCurrentUsuario(null);
+            }}
+          />
+        )}
+      </div>
+    </Suspense>
   );
 };
 
 export default MostrarUsuariosPage;
+
 
 
