68d32f118884e3108c248717e138eb428277ac7a
Rodrigo Iván Ordoñez Chávez <rodrigoordonez@nube.unadmexico.mx>
2025-05-20 18:43:22 -0600

    Ajuste de estilos en formulario login para evitar desbordes en diferentes pantallas

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -44,10 +44,14 @@ const LoginForm = () => {
     <>
       <Toaster position="top-right" />
       <NavBar />
-      <div className="flex items-center justify-center min-h-screen mt-10 mb-10">
-        <div className="bg-gray-100 flex flex-col sm:py-12 md:w-full md:max-w-3xl rounded-lg shadow-lg">
-          <div className="p-10 xs:p-0 mx-auto w-full">
-            <div className="px-5 py-7 text-center">
+      <div className="flex flex-col min-h-screen">
+        <div className="flex flex-1 items-center justify-center py-10">
+          {/* Contenedor principal */}
+          <div
+            className="bg-gray-100 w-full max-w-md rounded-lg shadow-lg p-6 sm:p-8"
+            style={{ maxHeight: "90vh" }}
+          >
+            <div className="text-center mb-6">
               <div className="flex justify-center mb-5">
                 <Image
                   src="/images/escudo-png.png"
@@ -59,80 +63,80 @@ const LoginForm = () => {
                 />
               </div>
 
-              <h1 className="font-black text-3xl mb-5 text-gold">
+              <h1 className="font-black text-3xl mb-5 text-blue">
                 Iniciar Sesión
               </h1>
+            </div>
 
-              <form
-                className="flex flex-col items-center"
-                onSubmit={handleSubmit(onSubmit)}
-                autoComplete="on"
+            <form
+              className="flex flex-col items-center"
+              onSubmit={handleSubmit(onSubmit)}
+              autoComplete="on"
+            >
+              <label
+                htmlFor="email"
+                className="font-semibold text-sm text-blue pb-1 block text-left w-full"
               >
-                <label
-                  htmlFor="email"
-                  className="font-semibold text-sm text-blue pb-1 block text-left w-full"
-                >
-                  Correo Electrónico
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  {...register("email", {
-                    required: "El correo es obligatorio",
-                  })}
-                  autoComplete="email"
-                  className="border border-yellow rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full text-blue focus:border-blue focus:ring-gold focus:ring-2 focus:outline-none"
-                  required
-                />
-                {errors.email && (
-                  <p className="text-red-500">{errors.email.message}</p>
-                )}
+                Correo Electrónico
+              </label>
+              <input
+                type="email"
+                id="email"
+                {...register("email", {
+                  required: "El correo es obligatorio",
+                })}
+                autoComplete="email"
+                className="border border-yellow rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full text-blue focus:border-blue focus:ring-gold focus:ring-2 focus:outline-none"
+                required
+              />
+              {errors.email && (
+                <p className="text-red-500">{errors.email.message}</p>
+              )}
 
-                <label
-                  htmlFor="login-password"
-                  className="font-semibold text-sm text-blue pb-1 block text-left w-full"
-                >
-                  Contraseña
-                </label>
-                <input
-                  type="password"
-                  id="login-password"
-                  {...register("password", {
-                    required: "La contraseña es obligatoria",
-                  })}
-                  autoComplete="current-password"
-                  className="border border-yellow rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full text-blue focus:border-blue focus:ring-gold focus:ring-2 focus:outline-none"
-                  required
-                />
-                {errors.password && (
-                  <p className="text-red-500">{errors.password.message}</p>
-                )}
+              <label
+                htmlFor="login-password"
+                className="font-semibold text-sm text-blue pb-1 block text-left w-full"
+              >
+                Contraseña
+              </label>
+              <input
+                type="password"
+                id="login-password"
+                {...register("password", {
+                  required: "La contraseña es obligatoria",
+                })}
+                autoComplete="current-password"
+                className="border border-yellow rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full text-blue focus:border-blue focus:ring-gold focus:ring-2 focus:outline-none"
+                required
+              />
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
 
-                {error && <p className="text-red-500">{error}</p>}
+              {error && <p className="text-red-500">{error}</p>}
 
-                <button
-                  type="submit"
-                  className={`transition duration-200 bg-yellow text-blue hover:bg-blue hover:text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block ${
-                    loading ? "opacity-50 cursor-not-allowed" : ""
-                  }`}
-                  disabled={loading}
-                >
-                  {loading ? "Cargando..." : "Iniciar Sesión"}
-                </button>
-              </form>
+              <button
+                type="submit"
+                className={`transition duration-200 bg-yellow text-blue hover:bg-blue hover:text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block ${
+                  loading ? "opacity-50 cursor-not-allowed" : ""
+                }`}
+                disabled={loading}
+              >
+                {loading ? "Cargando..." : "Iniciar Sesión"}
+              </button>
+            </form>
 
-              <div className="text-center mt-4 text-sm">
-                <Link href="#" className="text-blue hover:text-gold">
-                  ¿Olvidaste tu contraseña?
-                </Link>
-              </div>
+            <div className="text-center mt-4 text-sm">
+              <Link href="#" className="text-blue hover:text-gold">
+                ¿Olvidaste tu contraseña?
+              </Link>
             </div>
 
             <div className="text-center py-4">
               <span className="text-blue-900">o</span>
             </div>
 
-            <div className="p-5 flex justify-between space-x-3">
+            <div className="flex flex-col sm:flex-row gap-2">
               <button className="transition duration-200 border border-gray-200 text-gray-900 w-full py-2.5 rounded-lg text-sm hover:bg-gray-100 hover:shadow-lg flex items-center justify-center">
                 <i className="fab fa-google mr-2 text-red-600"></i>
                 <span>Google</span>
@@ -144,8 +148,8 @@ const LoginForm = () => {
             </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 };
