a088c2546eb8de188a0dc98b2838854a127910d5
Rodrigo Iván Ordoñez Chávez <rodrigoordonez@nube.unadmexico.mx>
2025-08-02 11:54:55 -0600

    Configuracion de toastUtils

--- src/lib/toastUtils.js
+++ src/lib/toastUtils.js
@@ -6,3 +6,8 @@ export const toastSuccess = (message) => toast.success(message);
 export const toastError = (message) => toast.error(message);
 
 export const toastInfo = (message) => toast(message);
+
+export const toastLoading = (message) =>
+  toast.loading(message, {
+    duration: 4000, // o ajusta a null para mantenerlo hasta que se remueva manualmente
+  });
